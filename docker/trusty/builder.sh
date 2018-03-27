a316a40540a4b3764a9845039534a402dfe14c08
Scott R. Shinn <scott@atomicorp.com>
2018-03-27 13:26:12 -0400

    Adding Kali support
    Signed-off-by: Scott R. Shinn <scott@atomicorp.com>

--- docker/trusty/builder.sh
+++ docker/trusty/builder.sh
@@ -12,7 +12,7 @@ source package/version
 CI_PIPELINE_ID=$(cat package/release)
 
 pushd package
-	dch -c debian/changelog  -b -v $DEB_VERSION-$CI_PIPELINE_ID -D $(awk -F'=' '/_CODENAME/{print$NF}' /etc/lsb-release)  "$CHANGELOG"
+	dch -c debian/changelog  -b -v $DEB_VERSION-$CI_PIPELINE_ID$DIST -D $(awk -F'=' '/_CODENAME/{print$NF}' /etc/lsb-release)  "$CHANGELOG"
 
 	fakeroot debian/rules clean
 	fakeroot debian/rules binary
